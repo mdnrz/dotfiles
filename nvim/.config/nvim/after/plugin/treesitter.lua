0e8b34abe3dd85b4e61b9e3da298553375c2abc8
mehdi-norouzi <mehdeenoroozi@gmail.com>
2023-06-25 21:10:04 +0330

    nvim: treesitter: install comment parser

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "c", "lua", "rust", "markdown" },
+  ensure_installed = { "help", "c", "lua", "rust", "markdown", "comment" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

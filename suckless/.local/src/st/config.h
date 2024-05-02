2a57fa8d7153665c34c0b745406980031883dad0
mehdi-norouzi <mehdeenoroozi@gmail.com>
2024-05-02 12:29:53 +0330

    suckless:st: use ctrl-shift as TERMMOD key

--- suckless/.local/src/st/config.h
+++ suckless/.local/src/st/config.h
@@ -133,7 +133,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#040404", /* 258 -> bg */
+	// "#040404", /* 258 -> bg */
+	"#151919", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
 };
 
@@ -262,32 +263,32 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
+/* 	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} }, */
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
-	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
+/* 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} }, */
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
-	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
-	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
-	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
-	{ MODKEY,		XK_s,		changealpha,	{.f = -0.05} },
-	{ MODKEY,		XK_a,		changealpha,	{.f = +0.05} },
+	{ TERMMOD,               XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ TERMMOD,               XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,               XK_K,           kscrollup,      {.i =  1} },
+	{ TERMMOD,               XK_J,           kscrolldown,    {.i =  1} },
+/* 	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} }, */
+/* 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} }, */
+	{ TERMMOD,               XK_U,           kscrollup,      {.i = -1} },
+	{ TERMMOD,               XK_D,           kscrolldown,    {.i = -1} },
+	{ TERMMOD,		        XK_plus,		    changealpha,	{.f = -0.05} },
+	{ TERMMOD,		        XK_underscore,		    changealpha,	{.f = +0.05} },
 	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
-	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
-	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
-	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
-	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
-	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
-	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+/* 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} }, */
+/* 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} }, */
+/* 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} }, */
+/*     { TERMMOD,              XK_D,           zoom,           {.f = -2} }, */
+	{ TERMMOD,               XK_L,           externalpipe,   {.v = openurlcmd } },
+	{ TERMMOD,               XK_Y,           externalpipe,   {.v = copyurlcmd } },
+	{ TERMMOD,               XK_O,           externalpipe,   {.v = copyoutput } },
 };
 
 /*

9907477636eeca7cbcc35dc4906a2b4303adb8fa
mehdi-norouzi <mehdeenoroozi@gmail.com>
2024-05-02 12:22:56 +0330

    nvim:telescope: add git-worktree dependency

--- nvim/.config/nvim/after/plugin/telescope-config.lua
+++ nvim/.config/nvim/after/plugin/telescope-config.lua
@@ -31,4 +31,5 @@ require('telescope').setup{
 
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
+require('telescope').load_extension('git_worktree')
 -- require('telescope').load_extension('lazygit')

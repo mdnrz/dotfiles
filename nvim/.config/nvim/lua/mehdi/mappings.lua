fb89e818e622703bfd1aa74030e1bc3acdcad9f1
mehdi-norouzi <mehdeenoroozi@gmail.com>
2024-07-22 11:41:28 +0330

    nvim: add keymap for date print

--- nvim/.config/nvim/lua/mehdi/mappings.lua
+++ nvim/.config/nvim/lua/mehdi/mappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")
 
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+
+-- print the date in current buffer
+vim.keymap.set("n", "<leader>n", "Go<Esc>:r!date<cr>0i## <Esc>o"--[[ :r!echo $(date)<cr>0i## <Esc>o" ]], opts)

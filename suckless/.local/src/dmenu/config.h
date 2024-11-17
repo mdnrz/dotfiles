d14e3195aa4004f93d6b0b4ed889743a2ee9078e
mehdi-norouzi <mehdeenoroozi@gmail.com>
2024-11-17 12:41:16 +0330

    dmenu: centered, single-line, wider

--- suckless/.local/src/dmenu/config.h
+++ suckless/.local/src/dmenu/config.h
@@ -2,6 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono-Regular:size=8",
@@ -12,9 +15,10 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#eeeeee", "#222e2e" },
+	[SchemeSel] = { "#ffffff", "#22212e" },
+	[SchemeOut] = { "#eeeeee", "#222e2e" },
+	// [SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/

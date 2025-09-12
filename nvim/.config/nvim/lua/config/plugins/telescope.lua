6f9908917a652402cad534e31ead661eafd78d6f
mdnrz <mehdeenoroozi@gmail.com>
2025-09-12 18:45:58 +0330

    add new keymaps for telescope

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,14 +1,37 @@
+local builtin = require("telescope.builtin")
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	-- or                              , branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-		vim.keymap.set('n', '<leader>fw', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fu', ':lua require("telescope.builtin").find_files({no_ignore=true})<CR>', {})
-	end
+
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          local utils = require("telescope.utils")
+          builtin.find_files({ cwd = utils.buffer_dir(), })
+        end,
+        desc = "Find files in buffer directory",
+      },
+      {
+        "<leader>gp",
+        function()
+          local utils = require("telescope.utils")
+          builtin.live_grep({ cwd = utils.buffer_dir(), })
+        end,
+        desc = "live grep in buffer directory",
+      },
+      { "<leader>ff", builtin.find_files, },
+      { "<leader>fg", builtin.live_grep, },
+      { "<leader>fb", builtin.buffers, },
+      { "<leader>fh", builtin.help_tags, },
+      { "<leader>fw", builtin.git_files, },
+      {
+        "<leader>fu",
+        function()
+          builtin.find_files({no_ignore = true, hidden = true })
+        end,
+      },
+    },
+
 }

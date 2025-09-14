a646da76f07fee05adebfa7caa9618013ed2145e
mdnrz <mehdeenoroozi@gmail.com>
2025-09-14 13:03:47 +0330

    nvim: add telescope keymap for outside directory

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,4 +1,18 @@
 local builtin = require("telescope.builtin")
+local outside_dir_path = nil
+
+function get_outside_dir()
+  if not outside_dir_path then
+    local input = vim.fn.input("Gimme the path: ", vim.fn.getcwd(), "dir")
+    if input ~= "" then
+      outside_dir_path = vim.fn.fnamemodify(input, ":p") -- nomalize to absolute path
+    else 
+      vim.notify("Empty path; Aborting.", vim.log.levels.WARN)
+      return
+    end
+  end
+end
+
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	-- or                              , branch = '0.1.x',
@@ -31,6 +45,23 @@ return {
         function()
           builtin.find_files({no_ignore = true, hidden = true })
         end,
+      },
+      {
+        "<leader>fo",
+        function()
+          get_outside_dir()
+          builtin.find_files({cwd = outside_dir_path})
+        end,
+        desc = "Find files in outside directory.",
+      },
+
+      {
+        "<leader>go",
+        function()
+          get_outside_dir()
+          builtin.live_grep({cwd = outside_dir_path})
+        end,
+        desc = "Find files in outside directory.",
       },
     },
 
